003bf3198866319cf7399b40187c650138a7418e
vlahosstenn <vlahosstenn@DESKTOP-Q0FCBP9>
2023-09-29 19:01:53 -0500

    Added docstrings and in-line comments for the stuff I wrote

--- ProductionCode/exoplanets.py
+++ ProductionCode/exoplanets.py
@@ -2,10 +2,17 @@ from math import *
 import csv 
 
 def take_exoplanet_data(exoplanetData) :
+    """
+    Takes a csv file of exoplanet data and returns a 
+    dictionary of planets keyed to lists containing 
+    all the info about them. 
+    """
+    #Make an empty dictionary
     exoplanetsByName = {}
 
     with open(exoplanetData, 'r') as csvfile:
         datareader = csv.DictReader(csvfile)
+        #Save all the important data from each planet
         for row in datareader:
             planetName = row['pl_name']
             hostName = row['hostname']
@@ -24,7 +31,9 @@ def take_exoplanet_data(exoplanetData) :
             galacticLatitude = row ['glat']
             galacticLongitude = row ['glon']
 
+            #Make that planet an entry in the dictionary
             if planetName not in exoplanetsByName :
+                #Put the data in a list 
                 exoplanetsByName[planetName] = [planetName, hostName, numberOfStars, numberOfPlanets,
                                                 numberOfMoons, discoveryMethod, discoveryYear, discoveryFacility, 
                                                 semiMajorAxis, planetRadius, planetMass, stellarRadius, stellarMass, 
@@ -36,31 +45,52 @@ def take_exoplanet_data(exoplanetData) :
 
 class exoplanetAnalyzer:
     def __init__(self):
+        """
+        Constructor for an Exoplanet Analyzer.
+        Uses an empty dictionary in place of a given one. 
+        """
         self.exoplanetDictionary = {}
     
     def __init__(self, dictionary): 
+        """
+        Constructor for an Exoplanet Analyzer.
+        Saves a given dictionary of planet info. 
+        """
         self.exoplanetDictionary = dictionary
 
     def create_dictionary_from_csv(self, file):
+        """ 
+        Takes a csv file of planet info,
+        and saves it as a dictionary for the Exoplanet Analyzer to use.
+        """
         self.exoplanetDictionary = take_exoplanet_data(file)
     
     def get_planet_info(self, planetName):
+        """Takes planet name and returns a list of info about that planet."""
         return self.exoplanetDictionary[planetName]
     
     def format_planet_info(self, exoplanetInfo):
+        """
+        Takes a list of planet info and returns a 
+        more readable string of data with appropriate labels.
+        """
+        #The data in all the lists are already ordered as follows:
         infoList = ["Planet Name: ", "Host Name: ", "Number of Stars: ", "Number of Planets: ",
                     "Number of Moons: ", "Discovery Method: ", "Discovery Year: ",
                     "Discovery Facility: ", "Semi-Major Axis: ", "Planet Radius: ", "Planet Mass: ",
                     "Stellar Radius: ", "Stellar Mass: ", "Stellar Luminosity: ", 
                     "Galactic Latitude: ", "Galactic Longitude: "]
-        formatedInfoString = "Planet Info \n"
+        
+        formatedInfoString = "Planet Info \n\n"
         s = 0
+        #Creates a big string, one label and variable a line
         while (s <= 15):
             formatedInfoString = formatedInfoString + infoList[s] + exoplanetInfo[s] + "\n"
             s += 1 
         return formatedInfoString
     
     def print_planet_info(self, planetName):
+        """ Takes planet name and prints all the info about it. """
         exoplanetInfo = self.get_planet_info(planetName)
         formatedExoplanetInfo = self.format_planet_info(exoplanetInfo)
         print(formatedExoplanetInfo)
