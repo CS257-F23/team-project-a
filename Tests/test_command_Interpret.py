68bcad86a0521701b8151f8fc4e5f16fce05643f
ochoaj <ochoaj@carleton.edu>
2023-10-02 18:21:02 -0500

    more tests finalized

--- Tests/test_command_Interpret.py
+++ Tests/test_command_Interpret.py
@@ -9,7 +9,6 @@ class TestCommandLineInterpreter(unittest.TestCase):
         cls.data = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
         cls.cli = Command_line_interpreter(cls.data)
 
-
     def test_run_planet_info_valid(self):
         planet_name = "11 Com b"
         result = self.cli.run_planet_info(planet_name)
@@ -21,23 +20,34 @@ class TestCommandLineInterpreter(unittest.TestCase):
         expected_error_message = f"No data available for desried planet"
         self.assertEqual(result, expected_error_message)
 
+    def test_run_planet_info_string(self):
 
+    def test_run_planet_info_special_char(self):
+
+
+    def test_run_goldilocks_planet_inside(self):
+        planet_name = "   "
+        result = self.cli.run_goldilocks_planet(planet_name)
+        self.assertEqual(result, "Planet is inside the goldilocks zone")
+
+    def test_run_goldilocks_planet_outsde(self):
+        planet_name = "     " 
+        result = self.cli.run_goldilocks_planet(planet_name)
+        self.assertEqual(result, "Planet is outside the goldilocks zone")
+
+
+
+
+    def test_run_goldilocks_planet_invaild(self):
+        planet_name = "FAKEPLANET"
+        result = self.capture_output(self.cli.run_goldilocks_planet, planet_name)
+        expected_error_message = "No data available for desired planet"
+        self.assertEqual(result, expected_error_message)
+    
+    
 
 
     def test_run_habitable_planets(self):
         result = self.cli.run_habitable_planets()
-        self.assertIsInstnce(result, list)
-
-    def test_run_goldilocks_planet_outside(self):
-
-    def test_run_goldilocks_planet_insde(self):
-
-    def test_run_goldoilocks_planet_invaild(self):
-        planet_name = "FAKEPLANET"
-        result = self.capture_output(self.cli.run_goldilocks_planet, planet)
-        expected_error_message = "No data available for desired planet"
-    
-    def test_run_specified_arg
-        
-        
+        self.assertIsInstance(result, list)
 
