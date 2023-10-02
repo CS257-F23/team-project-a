df011fb7c9169f970f8494ac1a7729687c284380
Allie <hoppera2@carleton.edu>
2023-10-01 20:59:31 -0500

    added Habitable_Finder and made some changes to fix missing info, also added test cases to main

--- ProductionCode/exoplanets.py
+++ ProductionCode/exoplanets.py
@@ -197,57 +197,12 @@ class Habitable_Finder:
         for planet in list:
             print(planet)
 
-class Command_line_interpreter:
-    def __init__(self) -> None:
-        """
-        Constructor for a Command Line Interpreter.
-        Uses an empty dictionary in place of a given one. 
-        """
-        self.exoplanetDictionary = {}
-
-    def __init__(self, dict):
-        """
-        Constructor for a Command Line Interpreter.
-        Saves a given dictionary of planet info. 
-        """
-        self.exoplanetDictionary = dict
-
-    def run_planet_info(self, planet_name):
-        " Takes a planet name and runs print_planet_info on that planet"
-        exoplanetFinder = exoplanetAnalyzer(self.exoplanetDictionary)
-        exoplanetFinder.print_planet_info(planet_name)
-
-    def run_goldilocks_planet(self, planet_name):
-        " Takes a planet name and runs print_goldilocks on that planet"
-        goldilocks = Goldilocks_Determiner(self.exoplanetDictionary)
-        goldilocks.print_goldilocks_zone(planet_name)
-    
-    def run_habitable_planets(self):
-        " Runs print_habitable_list"
-        hab_finder = Habitable_Finder(self.exoplanetDictionary)
-        hab_finder.print_habitable_list()
-
-        
-    def run_specified_arg(self):
-        " If args are valid, runs the specified function, else prints usage statement"
-        if sys.argv[1] == "--habitable_planets":
-            self.run_habitable_planets()
-
-        elif len(sys.argv) >= 3: 
-            if sys.argv[1] == "--planet_info":
-                self.run_planet_info(sys.argv[2])
-
-            elif sys.argv[1] == "--goldilocks_planet":
-                self.run_goldilocks_planet(sys.argv[2])
-
-        else:
-            print("exoplanet.py Usage: usage statement")
-
-
-def main():
-    exoplanetDictionary = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
-    command_line_interpreter = Command_line_interpreter(exoplanetDictionary)
-    command_line_interpreter.run_specified_arg()
-
 if __name__ == "__main__" :
-    main()
+    #test stuff
+    exoplanetDictionary = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
+    exo_analyzer = exoplanetAnalyzer(exoplanetDictionary)
+    gold_det = Goldilocks_Determiner(exoplanetDictionary)
+    hab_finder = Habitable_Finder(exoplanetDictionary)
+    exo_analyzer.print_planet_info('WASP-41 c')
+    gold_det.print_goldilocks_zone('14 Her b')
+    hab_finder.print_habitable_list()
