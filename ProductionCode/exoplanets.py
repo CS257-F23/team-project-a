69086cdcd2da4f0fca8edd01d47eaab95b538ef6
Allie <hoppera2@carleton.edu>
2023-10-01 20:41:00 -0500

    added Habitable_Finder and finished Command_line

--- ProductionCode/exoplanets.py
+++ ProductionCode/exoplanets.py
@@ -1,5 +1,6 @@
 from math import *
 import csv 
+import sys
 
 def take_exoplanet_data(exoplanetData) :
     """
@@ -99,24 +100,36 @@ class exoplanetAnalyzer:
 
 class Goldilocks_Determiner:
     def __init__(self) -> None:
-        pass
+        """
+        Constructor for a Goldilocks Determiner.
+        Uses an empty dictionary in place of a given one. 
+        """
+        self.exoplanetDictionary = {}
 
     def __init__(self, dict):
+        """
+        Constructor for a Goldilocks Determiner.
+        Saves a given dictionary of planet info. 
+        """
         self.exoplanetDictionary = dict
 
     def get_stellar_lum(self, planet_name):
         """ Takes planet name and returns it's stellar luminosity"""
         planet_info = self.exoplanetDictionary[planet_name]
-        stellar_lum = 10 ** float(planet_info[13])
-        
-        return float(stellar_lum)
+        if planet_info[13] != '':
+            stellar_lum = 10 ** float(planet_info[13])
+            return float(stellar_lum)
+        else:
+            return 0
 
     def get_sm_axis(self, planet_name):
         """ Takes planet name and returns it's semi-major axis"""
         planet_info = self.exoplanetDictionary[planet_name]
-        sm_axis = planet_info[8]
-        
-        return float(sm_axis)
+        if planet_info[8] != '':
+            sm_axis = planet_info[8]
+            return float(sm_axis)
+        else:
+            return -1
 
     def determine_goldilocks_inner(self, planet_name):
         """ Takes planet name and uses returns an ordered pair (inner, outer) bound"""
@@ -143,17 +156,92 @@ class Goldilocks_Determiner:
             return True
         else:
             return False
+        
+    def print_goldilocks_zone(self, planet_name):
+        if self.is_in_goldilocks_zone(planet_name):
+            print(planet_name, 'is in the goldilocks zone! (by Solar Equivalent AU)')
+        else:
+            print(planet_name, 'is not in the goldilocks zone (by Solar Equivalent AU).')
+
+class Habitable_Finder:
+    def __init__(self) -> None:
+        """
+        Constructor for a Habitable Finder.
+        Uses an empty dictionary in place of a given one. 
+        """
+        self.exoplanetDictionary = {}
+
+    def __init__(self, dict):
+        """
+        Constructor for a Habitable Finder.
+        Saves a given dictionary of planet info. 
+        """
+        self.exoplanetDictionary = dict
+
+    def create_list(self):
+        habitable_planets = []
+        goldilocks_det = Goldilocks_Determiner(self.exoplanetDictionary)
+        for planet in self.exoplanetDictionary:
+            if goldilocks_det.is_in_goldilocks_zone(planet):
+                habitable_planets.append(planet)
+        return habitable_planets
+    
+    def print_list(self):
+        list = self.create_list()
+        print("The habitable planets (by Solar Equivalent AU) found in this database are")
+        for planet in list:
+            print(planet)
+
+class Command_line_interpreter:
+    def __init__(self) -> None:
+        """
+        Constructor for a Command Line Interpreter.
+        Uses an empty dictionary in place of a given one. 
+        """
+        self.exoplanetDictionary = {}
+
+    def __init__(self, dict):
+        """
+        Constructor for a Command Line Interpreter.
+        Saves a given dictionary of planet info. 
+        """
+        self.exoplanetDictionary = dict
+
+    def run_planet_info(self, planet_name):
+        " Takes a planet name and runs print_planet_info on that planet"
+        exoplanetFinder = exoplanetAnalyzer(self.exoplanetDictionary)
+        exoplanetFinder.print_planet_info(planet_name)
+
+    def run_goldilocks_planet(self, planet_name):
+        " Takes a planet name and runs print_goldilocks on that planet"
+        goldilocks = Goldilocks_Determiner(self.exoplanetDictionary)
+        goldilocks.print_goldilocks_zone(planet_name)
+    
+    def run_habitable_planets(self):
+        hab_finder = Habitable_Finder(self.exoplanetDictionary)
+        hab_finder.print_list()
+
+        
+    def run_specified_arg(self):
+        " If first arg is valid, runs the specified function, else prints usage statement"
+        if sys.argv[1] == "--habitable_planets":
+            self.run_habitable_planets()
+
+        elif len(sys.argv) > 3: 
+            if sys.argv[1] == "--planet_info":
+                self.run_planet_info(sys.argv[2])
+
+            elif sys.argv[1] == "--goldilocks_planet":
+                self.run_goldilocks_planet(sys.argv[2])
+
+        else:
+            print("exoplanet.py Usage: usage statement 2")
+
+
+def main():
+    exoplanetDictionary = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
+    command_line_interpreter = Command_line_interpreter(exoplanetDictionary)
+    command_line_interpreter.run_specified_arg()
 
 if __name__ == "__main__" :
-    exoplanetDictionary = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
-    exoplanetFinder = exoplanetAnalyzer(exoplanetDictionary)
-    exoplanetFinder.print_planet_info('14 Her b')
-    goldilocks = Goldilocks_Determiner()
-    test1 = goldilocks.is_in_goldilocks_zone("Kepler-22 b")
-    test2 = goldilocks.is_in_goldilocks_zone("TRAPPIST-1 e")
-    test3 = goldilocks.is_in_goldilocks_zone("GJ 667 C f")
-    test4 = goldilocks.is_in_goldilocks_zone("Proxima Cen b")
-    print(test1)
-    print(test2)
-    print(test3)
-    print(test4)
+    main()
