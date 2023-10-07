21d8c2fc02819ff17c1a45ccbf2db4703c2931c9
ochoaj <ochoaj@carleton.edu>
2023-10-07 17:27:18 -0500

    updates

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from ProductionCode.Goldilocks import Goldilocks_Determiner
 from ProductionCode.load_data import take_exoplanet_data
 
 
-app = Flask (__name__)
+app = Flask(__name__)
 exoplanet_data = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
 goldilocks = Goldilocks_Determiner(exoplanet_data)
 
@@ -11,6 +11,11 @@ goldilocks = Goldilocks_Determiner(exoplanet_data)
 def index():
     return render_template('index.html')
 
-@app.route('/check_goldilocks', )
-    def check_goldilocks():
-    planet_name = reqeust.form.get('planet_name')
+@app.route('/check_goldilocks', methods=['POST'] )
+def check_goldilocks():
+    planet_name = request.form.get('planet_name')
+    result = goldilocks.is_in_goldilocks_zone(planet_name)
+    return render_template ('result.html', planet_name = planet_name, result = result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
