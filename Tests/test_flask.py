44ea3493ac1fdc85749b1847f916ace48214a38a
Allie <hoppera2@carleton.edu>
2023-10-09 10:47:26 -0500

    fixed tests for flask

--- Tests/test_flask.py
+++ Tests/test_flask.py
@@ -1,7 +1,5 @@
-from app import *
 import unittest
-
-# This should be modfied for our team flask app!
+from app import *
 
 class TestHomepage(unittest.TestCase):
     def test_route(self):
@@ -27,26 +25,20 @@ class TestGoldilocksPlanet(unittest.TestCase):
     def test_14_Her_b_goldilocks_route(self):
         """Test that using 14 Her b with the goldilocks_planet route outputs the correct string"""
         self.app = app.test_client()
-        response = self.app.get('/goldilocks_planet/14 Her b', follow_redirects=True)
-        self.assertEqual(b'14 Her b is not in the Goldilocks Zone. (by Solar Equivalent AU)', response.data)
-    
+        response = self.app.get('/check_goldilocks/14 Her b', follow_redirects=True)
+        print(response.data)
+        self.assertIn(b'14 Her b is not in the Goldilocks Zone. (by Solar Equivalent AU)', response.data)
     def test_ups_And_d_goldilocks_route(self):
         """Test that using ups And d with the goldilocks_planet route outputs the correct string"""
         self.app = app.test_client()
-        response = self.app.get('/goldilocks_planet/ups And d', follow_redirects=True)
-        self.assertEqual(b'ups And d is in the Goldilocks Zone! (by Solar Equivalent AU)', response.data)
-
+        response = self.app.get('/check_goldilocks/ups And d', follow_redirects=True)
+        self.assertIn(b'ups And d is in the Goldilocks Zone! (by Solar Equivalent AU)', response.data)
 class TestErrorMessages(unittest.TestCase):
     def test_404(self):
         """Test that the 404 message appears when a nonexistant url is entered."""
         self.app = app.test_client()
         response = self.app.get('/gildilicks_plinet/down_or_d', follow_redirects=True)
         self.assertIn(b'far out in space, buddy!', response.data)
-
-    def test_404(self):
-        """Test that the 500 message appears when the code breaks."""
-        response = page_not_found(500)
-        self.assertIn(b'Housten we have a problem', response)    
 
 if __name__ == '__main__':
     unittest.main()
