595dce7364962d1fadd49b0ef8862a20acd0ad70
Allie <hoppera2@carleton.edu>
2023-10-10 22:53:22 -0500

    playing around with tests

--- Tests/test_Planet_analyzer.py
+++ Tests/test_Planet_analyzer.py
@@ -1,86 +1,116 @@
 import unittest
-import argparse
-from ProductionCode.Goldilocks import Goldilocks_Determiner
-from ProductionCode.HabitablePlanets import Habitable_Finder
 from ProductionCode.PlanetAnalyzer import exoplanetAnalyzer
 from ProductionCode.load_data import take_exoplanet_data
 
 
-class TestPlanetMethods(unittest.TestCase):
+class TestGetPlanetInfo(unittest.TestCase):
 
     def setUp(self):
         self.data = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
-        self.cli = Command_line_interpreter(self.data)
+        self.analyzer = exoplanetAnalyzer(self.data)
 
-    def test_run_planet_info_valid(self):
+    def test_get_info_valid(self):
         planet_name = "11 Com b"
-        result = self.cli.run_planet_info(planet_name)
+        result = self.analyzer.get_planet_info(planet_name)
         self.assertIn(planet_name, result)
 
-    def test_run_planet_info_invalid(self):
+    def test_get_info_invalid(self):
         planet_name = "FAKEPLANET"
-        result = self.cli.run_planet_info(planet_name)
-        expected_error_message = f"No data available for desried planet"
-        self.assertEqual(result, expected_error_message)
+        self.assertRaises(KeyError, self.analyzer.get_planet_info, planet_name)
 
-    def test_run_planet_info_string(self):
+    def test_get_info_string(self):
         #test function's capactiy to handle random strings
         planet_name = "randomSTRINGNOTaPLANET"
-        result = self.cli.run_planet_info(planet_name)
-        expected_error_message = f"No data available for desired planet"
-        self.assertEqual(result, expected_error_message)
+        self.assertRaises(KeyError, self.analyzer.get_planet_info, planet_name)
 
-    def test_run_planet_info_special_char(self):
+    def test_get_info_special_char(self):
         #test function's capcity to handle strings w/ special characters
         planet_name = "!@#$%^&*()"
-        result = self.cli.run_planet_info(planet_name)
-        expected_error_message = f"No data available for desired planet"
-        self.assertEqual(result, expected_error_message)
+        self.assertRaises(KeyError, self.analyzer.get_planet_info, planet_name)
 
-    def test_run_planet_info_none(self):   
-        result = self.cli.run_planet_info(None)
-        expected_error_message = f'No data available for desired planet'
-        self.assertEqual(result, expected_error_message)
+    def test_get_info_none(self):   
+        self.assertRaises(KeyError, self.analyzer.get_planet_info, None)
 
-    def test_run_planet_info_empty(self):
-        result = self.cli.run_planet_info("")
-        expected_error_message = f'No data available for desired planet'
-        self.assertEqual(result, expected_error_message)
+    def test_get_info_empty(self):
+        self.assertRaises(KeyError, self.analyzer.get_planet_info, "")
 
-    def test_run_planet_info_non_string(self):
-        result = self.cli.run_planet_info(354)
-        expected_error_message = f'No data available for desired planet'
-        self.assertEqual(result, expected_error_message)
+    def test_get_info_non_string(self):
+        self.assertRaises(KeyError, self.analyzer.get_planet_info, 354)
 
-    def test_run_planet_info_whitespaces(self):
-        result = self.cli.run_planet_info("       ")
-        expected_error_message = f'No data available for desired planet'
-        self.assertEqual(result, expected_error_message)
+    def test_get_info_whitespaces(self):
+        self.assertRaises(KeyError, self.analyzer.get_planet_info, "       ")
 
-    def test_run_goldilocks_planet_inside(self):
-        planet_name = "Kepler-186 f"
-        result = self.cli.run_goldilocks_planet(planet_name)
-        self.assertEqual(result, "Planet is inside the goldilocks zone")
 
-    def test_run_goldilocks_planet_outsde(self):
-        planet_name = "Kepler-1777 b" 
-        result = self.cli.run_goldilocks_planet(planet_name)
-        self.assertEqual(result, "Planet is outside the goldilocks zone")
+class TestFormatPlanetInfo(unittest.TestCase):
 
-    def test_run_goldilocks_planet_invaild(self):
+    def setUp(self):
+        self.data = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
+        self.analyzer = exoplanetAnalyzer(self.data)
+
+    def test_format_info_valid(self):
+        planet_name = "11 Com b"
+        result = self.analyzer.format_planet_info(planet_name)
+        self.assertIn(planet_name, result)
+
+    def test_format_info_invalid(self):
         planet_name = "FAKEPLANET"
-        result = self.capture_output(self.cli.run_goldilocks_planet, planet_name)
-        expected_error_message = "No data available for desired planet"
-        self.assertEqual(result, expected_error_message)
+        self.assertRaises(IndexError, self.analyzer.format_planet_info, planet_name)
 
-    def test_run_habitable_planets(self):
-        result = self.cli.run_habitable_planets()
-        self.assertIsInstance(result, list)
+    def test_format_info_string(self):
+        #test function's capactiy to handle random strings
+        planet_name = "randomSTRINGNOTaPLANET"
+        self.assertRaises(IndexError, self.analyzer.format_planet_info, planet_name)
 
-    def test_extract_info_and_goldilocks_intergration(self):
-        planet_name = "Kepler-186 f"
-        info_result = self.chi.run_planet_info(planet_name)
-        self.assertIn(planet_name, info_result)
-        goldilocks_status = self.cli.run_goldilocks_planet(planet_name)
-        self.assertEqual(goldilocks_status, "Planet is inside the goldilocks zone")
-        
+    def test_format_info_special_char(self):
+        #test function's capcity to handle strings w/ special characters
+        planet_name = "!@#$%^&*()"
+        self.assertRaises(IndexError, self.analyzer.format_planet_info, planet_name)
+
+    def test_format_info_none(self):   
+        self.assertRaises(IndexError, self.analyzer.format_planet_info, None)
+
+    def test_format_info_empty(self):
+        self.assertRaises(IndexError, self.analyzer.format_planet_info, "")
+
+    def test_format_info_non_string(self):
+        self.assertRaises(TypeError, self.analyzer.format_planet_info, 972)
+
+    def test_format_info_whitespaces(self):
+        self.assertRaises(IndexError, self.analyzer.format_planet_info, "      ")
+
+class TestGetFormattedInfo(unittest.TestCase):
+
+    def setUp(self):
+        self.data = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
+        self.analyzer = exoplanetAnalyzer(self.data)
+
+    def test_get_formatted_info_valid(self):
+        planet_name = "11 Com b"
+        result = self.analyzer.get_formatted_planet_info(planet_name)
+        self.assertIn(planet_name, result)
+
+    def test_get_formatted_info_invalid(self):
+        planet_name = "FAKEPLANET"
+        self.assertRaises(KeyError, self.analyzer.get_formatted_planet_info, planet_name)
+
+    def test_get_formatted_info_string(self):
+        #test function's capactiy to handle random strings
+        planet_name = "randomSTRINGNOTaPLANET"
+        self.assertRaises(KeyError, self.analyzer.get_formatted_planet_info, planet_name)
+
+    def test_get_formatted_info_special_char(self):
+        #test function's capcity to handle strings w/ special characters
+        planet_name = "!@#$%^&*()"
+        self.assertRaises(KeyError, self.analyzer.get_formatted_planet_info, planet_name)
+
+    def test_get_formatted_info_none(self):  
+        self.assertRaises(KeyError, self.analyzer.get_formatted_planet_info, None)
+
+    def test_get_formatted_info_empty(self):
+        self.assertRaises(KeyError, self.analyzer.get_formatted_planet_info, "")
+
+    def test_get_formatted_info_non_string(self):
+        self.assertRaises(KeyError, self.analyzer.get_formatted_planet_info, 586)
+
+    def test_get_formatted_info_whitespaces(self):
+        self.assertRaises(KeyError, self.analyzer.get_formatted_planet_info, "      ")
--- Tests/test_exoplanets.py
+++ Tests/test_exoplanets.py
@@ -1,113 +1,85 @@
+import subprocess
 import unittest
-import argparse
-from ProductionCode.Goldilocks import Goldilocks_Determiner
-from ProductionCode.HabitablePlanets import Habitable_Finder
-from ProductionCode.PlanetAnalyzer import exoplanetAnalyzer
 from ProductionCode.load_data import take_exoplanet_data
-class TestCommandLineInterpreter(unittest.TestCase):
+from ProductionCode.exoplanets import *
 
-    @classmethod
-    def setUpClass(cls):
-        cls.data = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
-        cls.cli = Command_line_interpreter(cls.data)
-
-    def test_default_intialization(self):
-        cli = Command_line_interpreter()
-        self.assertIsInstance(cli.exoplanetDictionary, dict)
-        self.assertEqual(len(cli.exoplanetDictionary), 0, "should be empty")
-
-    def test_initialization_with_dict(self):
-        my_data = {
-            "Planet1": {"info1": "data1", "info2": "data2"},
-            "Planet2": {"info1": "data3", "info2": "data4"}
-        }
-        cli = Command_line_interpreter(my_data)
-        self.assertEqual(cli.exoplanetDictionary, my_data, "dictionary should be equal to exoplanet Dictionry")
-
-
-    def test_initialization_with_non_dict(self):
-        #raise TypeError if any structure other than a dictionary is passed
-        with self.assertRaises(TypeError):
-            cli = Command_line_interpreter("Not a dictionary")
-
-if __name__ == "__main__":
-    unittest.main()
 
 class TestPlanetMethods(unittest.TestCase):
 
     def setUp(self):
         self.data = take_exoplanet_data('Data/ExoplanetSimplifiedData.csv')
-        self.cli = Command_line_interpreter(self.data)
 
     def test_run_planet_info_valid(self):
-        planet_name = "11 Com b"
-        result = self.cli.run_planet_info(planet_name)
-        self.assertIn(planet_name, result)
+        code = subprocess.Popen(['python3', '-u', "ProductionCode/exoplanets.py", '--planet_info', '11 Com b'],
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                encoding='utf8')
+        output, err = code.communicate() 
+        self.assertIn('11 Com b', output.strip())
 
     def test_run_planet_info_invalid(self):
         planet_name = "FAKEPLANET"
-        result = self.cli.run_planet_info(planet_name)
+        result = run_planet_info(planet_name)
         expected_error_message = f"No data available for desried planet"
-        self.assertEqual(result, expected_error_message)
+        self.assert(result, expected_error_message)
 
     def test_run_planet_info_string(self):
         #test function's capactiy to handle random strings
         planet_name = "randomSTRINGNOTaPLANET"
-        result = self.cli.run_planet_info(planet_name)
+        result = run_planet_info(planet_name)
         expected_error_message = f"No data available for desired planet"
         self.assertEqual(result, expected_error_message)
 
     def test_run_planet_info_special_char(self):
         #test function's capcity to handle strings w/ special characters
         planet_name = "!@#$%^&*()"
-        result = self.cli.run_planet_info(planet_name)
+        result = run_planet_info(planet_name)
         expected_error_message = f"No data available for desired planet"
         self.assertEqual(result, expected_error_message)
 
     def test_run_planet_info_none(self):   
-        result = self.cli.run_planet_info(None)
+        result = run_planet_info(None)
         expected_error_message = f'No data available for desired planet'
         self.assertEqual(result, expected_error_message)
 
     def test_run_planet_info_empty(self):
-        result = self.cli.run_planet_info("")
+        result = run_planet_info("")
         expected_error_message = f'No data available for desired planet'
         self.assertEqual(result, expected_error_message)
 
     def test_run_planet_info_non_string(self):
-        result = self.cli.run_planet_info(354)
+        result = run_planet_info(354)
         expected_error_message = f'No data available for desired planet'
         self.assertEqual(result, expected_error_message)
 
     def test_run_planet_info_whitespaces(self):
-        result = self.cli.run_planet_info("       ")
+        result = run_planet_info("       ")
         expected_error_message = f'No data available for desired planet'
         self.assertEqual(result, expected_error_message)
 
     def test_run_goldilocks_planet_inside(self):
         planet_name = "Kepler-186 f"
-        result = self.cli.run_goldilocks_planet(planet_name)
+        result = run_goldilocks_planet(planet_name)
         self.assertEqual(result, "Planet is inside the goldilocks zone")
 
     def test_run_goldilocks_planet_outsde(self):
         planet_name = "Kepler-1777 b" 
-        result = self.cli.run_goldilocks_planet(planet_name)
+        result = run_goldilocks_planet(planet_name)
         self.assertEqual(result, "Planet is outside the goldilocks zone")
 
     def test_run_goldilocks_planet_invaild(self):
         planet_name = "FAKEPLANET"
-        result = self.capture_output(self.cli.run_goldilocks_planet, planet_name)
+        result = self.capture_output(run_goldilocks_planet, planet_name)
         expected_error_message = "No data available for desired planet"
         self.assertEqual(result, expected_error_message)
 
     def test_run_habitable_planets(self):
-        result = self.cli.run_habitable_planets()
+        result = run_habitable_planets()
         self.assertIsInstance(result, list)
 
     def test_extract_info_and_goldilocks_intergration(self):
         planet_name = "Kepler-186 f"
         info_result = self.chi.run_planet_info(planet_name)
         self.assertIn(planet_name, info_result)
-        goldilocks_status = self.cli.run_goldilocks_planet(planet_name)
+        goldilocks_status = run_goldilocks_planet(planet_name)
         self.assertEqual(goldilocks_status, "Planet is inside the goldilocks zone")
         
