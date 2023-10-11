0dd6b7f32c6a62f4b842f23d37387c53904300a1
vlahosstenn <vlahosstenn@carleton.edu>
2023-10-10 21:14:30 -0500

    Refactored take_exoplanet_data

--- ProductionCode/load_data.py
+++ ProductionCode/load_data.py
@@ -13,32 +13,43 @@ def take_exoplanet_data(exoplanetData) :
 
     with open(exoplanetData, 'r') as csvfile:
         datareader = csv.DictReader(csvfile)
-        #Save all the important data from each planet
         for row in datareader:
-            planetName = row['pl_name']
-            hostName = row['hostname']
-            numberOfStars = row['sy_snum']
-            numberOfPlanets = row['sy_pnum']
-            numberOfMoons = row['sy_mnum']
-            discoveryMethod = row['discoverymethod']
-            discoveryYear = row['disc_year']
-            discoveryFacility = row['disc_facility']
-            semiMajorAxis = row ['pl_orbsmax']
-            planetRadius = row['pl_rade']
-            planetMass = row['pl_bmasse']
-            stellarRadius = row ['st_rad']
-            stellarMass = row['st_mass']
-            stellarLuminosity = row ['st_lum']
-            galacticLatitude = row ['glat']
-            galacticLongitude = row ['glon']
+            exoplanetsByName = add_row_of_data_to_dictionary(row, exoplanetsByName)
 
-            #Make that planet an entry in the dictionary
-            if planetName not in exoplanetsByName :
-                #Put the data in a list 
-                exoplanetsByName[planetName] = [planetName, hostName, numberOfStars, numberOfPlanets,
-                                                numberOfMoons, discoveryMethod, discoveryYear, discoveryFacility, 
-                                                semiMajorAxis, planetRadius, planetMass, stellarRadius, stellarMass, 
-                                                stellarLuminosity, galacticLatitude, galacticLongitude]
-                
-        
         return exoplanetsByName
+
+def add_row_of_data_to_dictionary(row, exoplanetsByName):
+    """
+    Helper function for take_exoplanet_data. 
+    Adds one planet's worth of info into a growing dictionary of info by planet.
+    Param: dictionary, dictionary 
+    Returns: dictionary
+    """
+    
+    #Save all the important data from each planet
+    planetName = row['pl_name']
+    hostName = row['hostname']
+    numberOfStars = row['sy_snum']
+    numberOfPlanets = row['sy_pnum']
+    numberOfMoons = row['sy_mnum']
+    discoveryMethod = row['discoverymethod']
+    discoveryYear = row['disc_year']
+    discoveryFacility = row['disc_facility']
+    semiMajorAxis = row ['pl_orbsmax']
+    planetRadius = row['pl_rade']
+    planetMass = row['pl_bmasse']
+    stellarRadius = row ['st_rad']
+    stellarMass = row['st_mass']
+    stellarLuminosity = row ['st_lum']
+    galacticLatitude = row ['glat']
+    galacticLongitude = row ['glon']
+
+    #Make that planet an entry in the dictionary
+    if planetName not in exoplanetsByName :
+        #Put the data in a list 
+        exoplanetsByName[planetName] = [planetName, hostName, numberOfStars, numberOfPlanets,
+                                        numberOfMoons, discoveryMethod, discoveryYear, discoveryFacility, 
+                                        semiMajorAxis, planetRadius, planetMass, stellarRadius, stellarMass, 
+                                        stellarLuminosity, galacticLatitude, galacticLongitude]
+    return exoplanetsByName
+                
