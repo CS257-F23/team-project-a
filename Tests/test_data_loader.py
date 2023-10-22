70b50026b512e6dabc1267b6da0bf8bc15c6f1e0
vlahosstenn <vlahosstenn@carleton.edu>
2023-10-21 20:21:13 -0500

    Wrote two tests

--- Tests/test_data_loader.py
+++ Tests/test_data_loader.py
@@ -1,1 +1,29 @@
 #should test verify name in database, and maybe just check that a couple planets end up in the database it creates?
+import unittest
+
+from ProductionCode.Exoplanet_Data_Loader import *
+
+
+class TestDataLoader(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Creates an Exoplanet Analyzer filled with data on which tests
+        can be run
+        """
+        self.data = data_loader('Data/ExoplanetSimplifiedData.csv')
+
+    def test_verify_name_in_database_valid(self):
+        """
+        Test that inputting a planet that's in the data set into
+        verity_name_in_database returns the boolean True
+        """
+        self.assertEqual(self.data.verify_name_in_database("Her 14 b"), True)
+
+    def test_verify_name_in_database_invalid(self):
+        """
+        Test that inputting a planet that's not in the data set into
+        verity_name_in_database returns the boolean False
+        """
+        self.assertEqual(self.data.verify_name_in_database("Her 14 b"), False)
+    
