e8db59c85474af9788debfe8006a0ad4804f0287
Allie <hoppera2@carleton.edu>
2023-11-07 13:40:14 -0600

    added more planet_analyzer tests

--- Tests/test_Planet_analyzer.py
+++ Tests/test_Planet_analyzer.py
@@ -3,7 +3,6 @@ import unittest
 from ProductionCode.PlanetAnalyzer import exoplanetAnalyzer
 from ProductionCode.Exoplanet_Data_Loader import data_loader
 
-#TODO: test basic get and list get methods
 
 class TestGetPlanetInfo(unittest.TestCase):
 
@@ -62,7 +61,6 @@ class TestGetPlanetInfo(unittest.TestCase):
     def test_get_info_whitespaces(self):
         """Test that inputting a string of blank spaces into get_planet_info raises an error"""
         self.assertRaises(KeyError, self.analyzer.get_planet_info, "       ")
-
 
 class TestGetFormattedPlanetInfo(unittest.TestCase):
 
@@ -127,5 +125,70 @@ class TestGetFormattedPlanetInfo(unittest.TestCase):
         into get_formatted_planet_info raises an error
         """
         self.assertRaises(KeyError, self.analyzer.get_formatted_planet_info, "      ")
+
+class TestGetFormattedPlanetInfoList(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Creates an Exoplanet Analyzer filled with data on which tests
+        can be run
+        """
+        self.data = data_loader('Data/ExoplanetSimplifiedData.csv')
+        self.analyzer = exoplanetAnalyzer(self.data.exoplanetsByName)
+
+    def test_get_format_info_valid(self):
+        """
+        Test that inputting a planet in the data set into get_formatted_planet_info
+        returns formatted info about that planet. 
+        """
+        planet_name = "14 Her b"
+        result = self.analyzer.get_formatted_planet_info_list(planet_name)
+        self.assertIn('Planet Name: ' + planet_name, result)
+
+    def test_get_format_info_invalid(self):
+        """
+        Test that inputting a planet not in the data set 
+        into get_formatted_planet_info raises an error
+        """
+        planet_name = "FAKEPLANET"
+        self.assertRaises(KeyError,self.analyzer.get_formatted_planet_info_list, planet_name)
+
+    def test_get_format_info_special_char(self):
+        """
+        Test that inputting a funky string with special characters 
+        into get_formatted_planet_info raises an error
+        """
+        #test function's capcity to handle strings w/ special characters
+        planet_name = "!@#$%^&*()"
+        self.assertRaises(KeyError,self.analyzer.get_formatted_planet_info_list, planet_name)
+
+    def test_get_format_info_none(self):   
+        """
+        Test that inputting nothing 
+        into get_formatted_planet_info raises an error
+        """
+        self.assertRaises(KeyError,self.analyzer.get_formatted_planet_info_list, None)
+
+    def test_get_format_info_empty(self):
+        """
+        Test that inputting an empty string 
+        into get_formatted_planet_info raises an error
+        """
+        self.assertRaises(KeyError,self.analyzer.get_formatted_planet_info_list, "")
+
+    def test_get_format_info_non_string(self):
+        """
+        Test that inputting a non-string (specifically tested with an integer) 
+        into get_formatted_planet_info raises an error
+        """
+        self.assertRaises(KeyError,self.analyzer.get_formatted_planet_info_list, 972)
+
+    def test_get_format_info_whitespaces(self):
+        """
+        Test that inputting a string made up of a bunch of blank spaces 
+        into get_formatted_planet_info raises an error
+        """
+        self.assertRaises(KeyError,self.analyzer.get_formatted_planet_info_list, "      ")
+
 
 
